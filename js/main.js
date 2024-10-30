2d9d09b02913f635c18ad11802e8cbd860c13397
patrickarmani <arm22001@byui.edu>
2024-10-30 16:00:01 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $nav=$('.nav');
+    $toggleCollapse=$('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function(){
+      $nav.toggleClass('collapse');
+    });
+
+    //owl-carousel for blog
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay: true,
+        autoplayTimeout:3000,
+        dots:false,
+        nav:true,
+        navText:[$('.owl-navegation .owl-nav-prev'),$('.owl-navegation .owl-nav-next')]
+    });
+}); 
+
+
