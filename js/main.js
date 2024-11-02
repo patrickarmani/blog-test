d030ce1485474e7c47f58276666b6ed3bcc7d9af
patrickarmani <arm22001@byui.edu>
2024-11-02 18:51:13 -0300

    second commit, 11 october, 2024

--- js/main.js
+++ js/main.js
@@ -12,12 +12,25 @@ $(document).ready(function(){
 
     $('.owl-carousel').owlCarousel({
         loop:true,
-        autoplay: true,
+        autoplay: false,
         autoplayTimeout:3000,
         dots:false,
         nav:true,
-        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+       
     });
-}); 
+
+
+// click to scroll top
+$('.move-up span').click(function () {
+  $('html, body').animate({
+      scrollTop: 0
+  }, 1000);
+})
+
+// AOS Instance
+AOS.init();
+
+});
 
 
