198caaec79a96f9d5b5da0a0dc074da5e532b43c
patrickarmani <arm22001@byui.edu>
2024-11-04 12:30:12 -0300

    fixed main.js adding a comma

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 const responsive  ={
+  0: {
+    items: 1
+  },
   320: {
     items: 1
+  },
+  560: {
+    items: 2
+  },
+  960: {
+    items: 3
   }
 } ;
 
 $(document).ready(function(){
 
-    $nav=$('.nav');
-    $toggleCollapse=$('.toggle-collapse');
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
 
     /** click event on toggle menu */
-    $toggleCollapse.click(function(){
+    $toggleCollapse.click(function () {
       $nav.toggleClass('collapse');
     });
 
